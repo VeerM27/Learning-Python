149dc44b2b57eb5df07b099c3ed1b5f56e098f09
Veer Mehta <mehtaveer2711@gmail.com>
2025-12-02 16:51:47 +0000

    For Loop file

---
+++ forloop.py
@@ -0,0 +1,9 @@
+numbers = [4, 13, 41, 46, 15, 76, 101, 6, 81, 55]
+
+largest_num = 0
+
+for i in numbers :
+    if i > largest_num :
+        largest_num = i
+
+print('The largest number is ', largest_num)
