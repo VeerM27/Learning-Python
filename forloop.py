d1d8289b20796a1db7eed20f8c61ac5130386399
Veer Mehta <mehtaveer2711@gmail.com>
2025-12-02 17:04:43 +0000

    Added Count

--- forloop.py
+++ forloop.py
@@ -1,9 +1,12 @@
 numbers = [4, 13, 41, 46, 15, 76, 101, 6, 81, 55]
 
 largest_num = 0
+count = 0
 
 for i in numbers :
     if i > largest_num :
         largest_num = i
+    count += 1
 
 print('The largest number is ', largest_num)
+print('Number of entries', count)
