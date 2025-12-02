cb2e8693f63495b37d493fd0d545376140b19a17
Veer Mehta <mehtaveer2711@gmail.com>
2025-12-02 17:11:55 +0000

    None functionality

--- forloop.py
+++ forloop.py
@@ -1,12 +1,14 @@
 numbers = [4, 13, 41, 46, 15, 76, 101, 6, 81, 55]
 
-largest_num = 0
+smallest_num = None
 count = 0
 
 for i in numbers :
-    if i > largest_num :
-        largest_num = i
+    if smallest_num is None :
+        smallest_num = i
+    if i < smallest_num :
+        smallest_num = i
     count += 1
 
-print('The largest number is ', largest_num)
-print(largest_num, 'was the largest out of the', count, 'entries.' )
+print('The smallest number is ', smallest_num)
+print(smallest_num, 'was the smallest out of the', count, 'entries.' )
