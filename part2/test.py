c56ffae431de71219dabbfcfb015542e63fb747b
Veer Mehta <mehtaveer2711@gmail.com>
2025-12-04 01:34:15 +0000

    Using Lists in conjunction with file work

--- part2/test.py
+++ part2/test.py
@@ -9,21 +9,10 @@ except :
 entries = []
 
 for line in fhand :
-    if line.startswith('X-DSPAM-Confidence:') :
+    if line.startswith('Author') :
         lines = line.rstrip()
-        num = lines[20:28]
-        entries.append(num)
-
-sum = 0
-count = 0
+        entries.append(lines)
 
 for i in entries :
-    j = float(i)
-    sum += j
-    count += 1
-
-print(sum, count)
-
-#for line in fhand :
- #   lines = line.rstrip()
-  #  print(lines)
+    pos = i.find("@")
+    print(i[pos+1:])
