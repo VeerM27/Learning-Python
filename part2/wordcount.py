d949dcfddc7c9ea4b09d63f11b69819bbd1bda3a
Veer Mehta <mehtaveer2711@gmail.com>
2025-12-08 01:25:53 +0000

    Working with dictionaries

---
+++ part2/wordcount.py
@@ -0,0 +1,23 @@
+fname = input('Enter filename: ')
+
+try:
+    fhand = open(fname)
+except :
+    print('File not found!')
+    quit()
+
+counts = dict()
+for line in fhand :
+    words = line.split()
+    for word in words :
+        counts[word] = counts.get(word, 0) + 1
+
+
+bigcount = None
+bigword = None 
+for word,count in counts.items() :
+    if bigcount is None or count > bigcount :
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
